422bf9290925f42bcf0c7a46f15c6ab1e342d180
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-27 20:31:04 +0600

    discussed about map, forEach, filter & find

---
+++ arrayMethods.js
@@ -0,0 +1,26 @@
+const products = [
+    { name: 'laptop', price: 32000, brand: 'lenevo', color: 'silver' },
+    { name: 'phone', price: 7000, brand: 'iPhone', color: 'golden' },
+    { name: 'watch', price: 3000, brand: 'samsung', color: 'black' },
+    { name: 'sunglass', price: 300, brand: 'ribon', color: 'black' },
+    { name: 'camera', price: 10000, brand: 'canon', color: 'gray' }
+];
+
+const brand = products.map(product => product.brand);
+const prices = products.map(product => product.price);
+// console.log(prices);
+
+// products.forEach(product => console.log(product));
+// products.forEach(product => console.log(product.brand));
+
+// 3. filter
+const cheap = products.filter(product => product.price <= 5000);
+// console.log(cheap);
+
+const specificName = products.filter(product => product.name.includes('p'));
+// console.log(specificName);
+
+const special = products.find(product => product.name.includes('n'));
+// console.log(special);
+
+// return array: map() & filter()  // doesn't return array: forEach() & find()
